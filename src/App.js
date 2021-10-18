543530fe785f11c136ce197e627867f6fc49bb10
Tedros <tedtfu@gmail.com>
2021-10-17 21:54:16 -0400

    css for images applied, favourite icon added, on click event for image added

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ function App() {
     setData(data);
   }, []);
 
+  const handleClick = async (id) => {
+    let val = localStorage.getItem(id) === "true" ? false : true;
+    localStorage.setItem(id, val);
+  };
+
   async function fetchData() {
     const url = `${BASE_URL}/photos?_start=${_start}&_limit=${_limit}`;
     let res = await axios.get(url);
@@ -47,7 +52,26 @@ function App() {
                 height="330"
                 src={dta.url}
                 alt={dta.title}
+                onClick={() => handleClick(dta.id)}
               />
+              <svg
+                viewBox="0 0 100 100"
+                style={{ marginTop: ".2em" }}
+                width="19"
+                height="19"
+                onClick={() => handleClick(dta.id)}
+              >
+                <path
+                  data-testid={`${dta.id}s`}
+                  stroke="#A03232"
+                  strokeWidth="3"
+                  fill={
+                    localStorage.getItem(dta.id) === "true" ? "red" : "white"
+                  }
+                  d="M50.3,87.7c2-4.3,9.5-12.8,18-18.8c10.2-7.2,26-13.2,25.9-30.2C94,7.6,60.9,2.6,50,28.3
+		C38.5,1.6,6,7.8,5.7,38.8c-0.1,17,15.5,23.4,25.9,30.2C41.7,75.7,48.9,84.7,50,88.5C50.1,88.3,50.2,88,50.3,87.7"
+                />
+              </svg>
             </div>
           ))
         ) : (
